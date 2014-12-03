873f5874c427947fc4be9d7e8f64e99c353bf71c
fernanj <joaquin.fernandez@experian.com>
2014-12-03 11:16:11 +0100

    first commit

---
+++ FactoresPrimos/Factores.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FactoresPrimos
+{
+  public class Factores
+  {
+  }
+}
---
+++ FactoresPrimosTests/FactoresTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FactoresPrimosTests
+{
+  public class FactoresTests
+  {
+  }
+}
